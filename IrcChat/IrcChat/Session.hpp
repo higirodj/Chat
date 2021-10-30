d2ca793a385aa6dd732f5144896ca241f2eb2690
higirodj <higirodj@users.noreply.github.com>
2021-10-30 13:31:56 -0400

    Check in Session class
    Checked in Session class that represents a context of a single TCP client connection.

--- IrcChat/IrcChat/Session.hpp
+++ IrcChat/IrcChat/Session.hpp
@@ -1,13 +1,93 @@
 //
-//  Session.hpp
-//  IrcChat
+//  Session.h
+//  ChatClient
 //
 //  Created by Julius Higiro on 10/30/21.
 //
+#pragma once
 
-#ifndef Session_hpp
-#define Session_hpp
+#include <string>
+#include <queue>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
 
-#include <stdio.h>
+using namespace boost::asio;
+using namespace boost::asio::ip;
+using namespace boost::system;
 
-#endif /* Session_hpp */
+namespace chat
+{
+    namespace irc
+    {
+        // Session represents the context for a single tcp client connection.
+        class Session
+        {
+        public:
+            Session(io_context& io_context);
+            virtual ~Session();
+            /**
+             * Performs an asynchronous connection to the server.
+             * Param 1: The hostname of listening server.
+             * Param 2: The port number of listening server.
+             * Param 3: A handler invoked during an asynchronous Session.
+             * Return none.
+             */
+            void asyncConnect(const std::string& host,
+                              const std::string& port,
+                              const std::function<void(const tcp::resolver::iterator &, const error_code &)> & handler = {});
+            /**
+             * Sets the read handler for the asynchronous read operation.
+             * Param 1: A handler invoked during an asynchronous read.
+             * Return none.
+             */
+            void asyncRead(const std::function<void(const std::string &, const error_code &)> & handler);
+            /**
+             * Performs an asynchronous write of messages to the server.
+             * Param 1: A message to send to the server.
+             * Param 2: A handler invoked during an asynchronous write.
+             * Return none.
+             */
+            void asyncWrite(const std::string& message, const std::function<void(const bool &)> & handler = {});
+            /**
+             * Closes the socket
+             * Param none.
+             * Return none.
+             */
+            void close();
+
+        private:
+            io_context& mIoContext;
+            tcp::resolver mResolver;
+            tcp::socket mSocket;
+            std::queue<std::string> mMessageQueue;
+            boost::asio::streambuf mReadBuffer;
+            std::function<void(const std::string &, const error_code &)> mReadHandler;
+            bool mIsResolved;
+            bool mIsConnected;
+            /**
+             * Performs an asynchronous connection to the server.
+             * Param 1: Resolver iterator to resolve hostname address.
+             * Param 2: A completion handler that is invoked when the async operation is completed.
+             * Return none.
+             */
+            void makeConnect(tcp::resolver::iterator iterator,
+                            const std::function<void(const tcp::resolver::iterator &, const error_code &)> & handler = {});
+            /**
+             * Performs an asynchronous read  operation to receive messages from the server.
+             * Param none.
+             * Return none.
+             */
+            void makeRead();
+            /**
+             * Performs an asynchronous write operation that sends messages to the server.
+             * Param none.
+             * Return none.
+             */
+            void makeWrite();
+        };
+    }
+}
+
+
+
+
