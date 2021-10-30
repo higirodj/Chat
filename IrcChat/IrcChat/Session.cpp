ed8edff1608d1a7a830eae1e62ed5cca4a6123a7
higirodj <julius.higiro@gmail.com>
2021-10-30 13:22:18 -0400

    added files to be ignored.

---
+++ IrcChat/IrcChat/Session.cpp
@@ -0,0 +1,8 @@
+//
+//  Session.cpp
+//  IrcChat
+//
+//  Created by Julius Higiro on 10/30/21.
+//
+
+#include "Session.hpp"
---
+++ IrcChat/IrcChat/Session.hpp
@@ -0,0 +1,13 @@
+//
+//  Session.hpp
+//  IrcChat
+//
+//  Created by Julius Higiro on 10/30/21.
+//
+
+#ifndef Session_hpp
+#define Session_hpp
+
+#include <stdio.h>
+
+#endif /* Session_hpp */
